d491ca70e303ed06fd740fd7d924c5ab6152d43a
AntonRev <anton.revutskij@yandex.ru>
2022-12-17 20:27:04 +0300

    Add test api films

--- src/tests/functional/src/test_search.py
+++ src/tests/functional/src/test_search.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import uuid
 from logging.config import dictConfig
 
 import pytest
@@ -11,25 +12,92 @@ from tests.functional.testdata.query_data import QueryData
 dictConfig(LOGGING)
 log = logging.getLogger(__name__)
 query = QueryData()
+ES_INDEX = 'movies'
 
 
+@pytest.mark.parametrize(
+    'query_data, expected_answer',
+    [
+        (
+                {'count': 50, 'size': 100, 'name': 'The Star'},
+                {'status': 200, 'length': 50}
+        ),
+        (
+                {'count': 50, 'size': 10, 'name': 'The Star'},
+                {'status': 200, 'length': 10}
+        ),
+        (
+                {'count': 50, 'size': 99999, 'name': 'The Star'},
+                {'status': 422, 'length': 1}
+        ),
+        (
+                {'count': 50, 'size': 0, 'name': 'The Star'},
+                {'status': 422, 'length': 1}
+        ),
+        (
+                {'count': 50, 'size': -1, 'name': 'The Star'},
+                {'status': 422, 'length': 1}
+        )
+
+    ]
+)
 @pytest.mark.asyncio
-async def test_search(fill_test_data, make_get_request, clear_elastic):
-    await clear_elastic
+async def test_search(fill_test_data, make_get_request, clear_elastic, query_data, expected_answer):
+    await clear_elastic(ES_INDEX)
     log.info('Elastic clear')
     # Генерируем данные для ES
-    await fill_test_data(es_index='movies', count=70, query_data=query.FILM)
+    await fill_test_data(es_index=ES_INDEX, count=query_data['count'], query_data=query.FILM)
     await asyncio.sleep(1)  # Pause to fill the database
 
     url = f'http://{test_settings.service_url}:8000/api/v1/films/search?'
-    query_data = {'query': 'The Star',
-                  'page[number]': '1',
-                  'page[size]': '50'
-                  }
+    query_datas = {'query': query_data['name'],
+                   'page[number]': '1',
+                   'page[size]': query_data['size']
+                   }
 
-    response = await make_get_request(url, query_data)
+    response = await make_get_request(url, query_datas)
+    resp = await response.json()
+
+    # Проверяем ответ
+    assert response.status == expected_answer['status']
+    assert len(resp) == expected_answer['length']
+
+
+@pytest.mark.asyncio
+async def test_film_id(fill_test_data, make_get_request):
+    # Генерируем данные для ES
+    id_film = str(uuid.uuid4())
+    await fill_test_data(es_index=ES_INDEX, count=1, query_data=query.FILM, es_id_field=id_film)
+    await asyncio.sleep(1)  # Pause to fill the database
+
+    url = f'http://{test_settings.service_url}:8000/api/v1/films/{id_film}'
+    response = await make_get_request(url)
+    resp = await response.json()
+    # Проверяем ответ
+    assert response.status == 200
+    assert resp['id'] == id_film
+
+
+@pytest.mark.asyncio
+async def test_film_redis(fill_test_data, make_get_request, clear_elastic):
+    # Генерируем данные для ES
+    id_film = str(uuid.uuid4())
+    await fill_test_data(es_index=ES_INDEX, count=1, query_data=query.FILM, es_id_field=id_film)
+    await asyncio.sleep(1)  # Pause to fill the database
+
+    url = f'http://{test_settings.service_url}:8000/api/v1/films/{id_film}'
+    response = await make_get_request(url)
     resp = await response.json()
 
     # Проверяем ответ
     assert response.status == 200
-    assert len(resp) == 50
+    assert resp['id'] == id_film
+
+    # Очистка Elastic для проверки Redis
+    await clear_elastic(ES_INDEX)
+    log.info('Elastic clear')
+    url = f'http://{test_settings.service_url}:8000/api/v1/films/{id_film}'
+    response = await make_get_request(url)
+    resp = await response.json()
+    assert response.status == 200
+    assert resp['id'] == id_film
